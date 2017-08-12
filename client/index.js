84f9480f5e3bbbbbf8cfc60d11ad5a4dcf1b5ea1
Tamara Piksa <tamara.piksa@gmail.com>
2017-08-11 18:20:21 -0700

    Add react

--- client/index.js
+++ client/index.js
@@ -1,5 +1,5 @@
-/*
-    ./client/index.js
-    which is the webpack entry file
-*/
-console.log('Hey guys and ladies!!')
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App.jsx';
+
+ReactDOM.render(<App />, document.getElementById('root'));
